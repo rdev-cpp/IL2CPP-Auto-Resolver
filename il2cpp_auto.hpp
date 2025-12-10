074c91495472ffa2aa59a0c8e8cf3f418fdefb59
RDev <mlrsrust1@gmail.com>
2025-12-10 19:48:12 +0500

    Updated

--- il2cpp_auto.hpp
+++ il2cpp_auto.hpp
@@ -4,18 +4,24 @@
 #include <unordered_map>
 #include <shared_mutex>
 #include <mutex>
+#include <atomic>
 
 namespace IL2CPP_Auto {
     class Resolver {
     private:
-        inline static HMODULE m_IL2CPPModule = nullptr;
+        inline static std::atomic<HMODULE> m_IL2CPPModule{ nullptr };
         inline static std::unordered_map<std::string, uintptr_t> m_Cache;
         inline static std::shared_mutex m_Mutex;
 
         static HMODULE GetModule() {
-            if (m_IL2CPPModule) return m_IL2CPPModule;
-            m_IL2CPPModule = GetModuleHandleA("GameAssembly.dll");
-            return m_IL2CPPModule;
+            HMODULE mod = m_IL2CPPModule.load(std::memory_order_acquire);
+            if (mod) return mod;
+
+            mod = GetModuleHandleA("GameAssembly.dll");
+            if (!mod) mod = GetModuleHandleA("UnityPlayer.dll");
+
+            if (mod) m_IL2CPPModule.store(mod, std::memory_order_release);
+            return mod;
         }
 
         static uintptr_t ResolveExport(const std::string& name) {
@@ -33,19 +39,22 @@ namespace IL2CPP_Auto {
             {
                 std::shared_lock<std::shared_mutex> lock(m_Mutex);
                 auto it = m_Cache.find(name);
-                if (it != m_Cache.end()) {
-                    return it->second;
-                }
+                if (it != m_Cache.end()) return it->second;
             }
 
             uintptr_t addr = ResolveExport(name);
-            if (addr == 0) return 0;
+            if (!addr) return 0;
 
             {
                 std::unique_lock<std::shared_mutex> lock(m_Mutex);
-                m_Cache[name] = addr;
+                auto it = m_Cache.find(name);
+                if (it == m_Cache.end()) {
+                    m_Cache[name] = addr;
+                }
+                else {
+                    addr = it->second;
+                }
             }
-
             return addr;
         }
 
@@ -57,7 +66,7 @@ namespace IL2CPP_Auto {
         static void ClearCache() {
             std::unique_lock<std::shared_mutex> lock(m_Mutex);
             m_Cache.clear();
-            m_IL2CPPModule = nullptr;
+            m_IL2CPPModule.store(nullptr, std::memory_order_release);
         }
     };
 }
